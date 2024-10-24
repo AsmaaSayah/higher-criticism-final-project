88d1bece943a4597013b1fd63ee7fd44094eba55
Asmaa Sayah <126309373+AsmaaSayah@users.noreply.github.com>
2024-10-24 13:43:57 +0000

    Added metrics

--- src/HC.py
+++ src/HC.py
@@ -9,15 +9,10 @@ nltk.data.path.append('./venv/lib/python3.8/site-packages/nltk_data')
 nltk.download('averaged_perceptron_tagger', download_dir='./venv/lib/python3.8/site-packages/nltk_data')
 nltk.download('universal_tagset', download_dir='./venv/lib/python3.8/site-packages/nltk_data')
 nltk.download('wordnet', download_dir='./venv/lib/python3.8/site-packages/nltk_data')
-
 import spacy
 # Load the model
 nlp = spacy.load("en_core_web_sm")
 
-# Function to filter adjectives
-def filter_adjectives_spacy(words):
-    doc = nlp(" ".join(words))
-    return [token.text for token in doc if token.pos_ == "ADJ"]
 
 def calculate_hc(human_data, ai_data, gamma_0=0.35):
     """
@@ -38,11 +33,10 @@ def calculate_hc(human_data, ai_data, gamma_0=0.35):
     # Compute word frequencies for human and AI sentences.
     human_freq = pd.Series([word for sentence in human_data['human_sentence'] for word in sentence]).value_counts()
     ai_freq = pd.Series([word for sentence in ai_data['ai_sentence'] for word in sentence]).value_counts()
-
     # Calculate total word counts in each set of sentences.
     n1 = human_freq.sum()
     n2 = ai_freq.sum()
-    
+
     # Combine vocabulary from both human and AI word frequencies.
     vocabulary = set(human_freq.index) | set(ai_freq.index)
     N = len(vocabulary)
@@ -70,17 +64,15 @@ def calculate_hc(human_data, ai_data, gamma_0=0.35):
     
     # Find the maximum HC score and its index.
     i_star = imin + np.argmax(hc_scores)
-    hc_threshold = sorted_pis[i_star]
-    print(hc_threshold)
-    
+    hc_threshold = sorted_pis[i_star]    
     # Select words whose p-values are below the HC threshold.
     hc_words = [w for w, pi in pi_values if pi <= hc_threshold]
     
     # Filter words that are more frequent in AI data compared to human data.
     more_frequent_in_ai = [w for w in hc_words if ai_freq.get(w, 0) > human_freq.get(w, 0)]
-    
+
     # Return the number of discriminating words, words more frequently used by AI, and a subset of hc_words.
-    return len(hc_words), hc_words[:100], more_frequent_in_ai
+    return hc_threshold, len(hc_words), more_frequent_in_ai, N
 
 
 
